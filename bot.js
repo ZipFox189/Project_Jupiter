cc7e43f373b40685a32e4b3c4e5a5ef730898e16
ZipFox189 <46225636+ZipFox189@users.noreply.github.com>
2020-04-03 23:09:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const { Client, Attachment, RichEmbed } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const quiz = require('./quiz/quiz.json');
 const item = quiz[Math.floor(Math.random() * quiz.length)];
 const filter = response => {
 	return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
 };
 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
+/*
 client.on('message', msg => {
   if (msg.content === '!pj') {
     msg.reply('\nКоманды: !servers !owner !about !quiz !link \nАдмин команды: !say !event !clear\nГифки: !win !nyancat !nice !fuckyou');
@@ -36,7 +36,7 @@ client.on('message', message => {
 
 client.on('message', msg => {
   if (msg.content === '!event') {
-    if (msg.member.roles.find(r => r.name === "Event Manager")) {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
         msg.channel.send('Внимание вопрос!\nХотите ли вы ивент?')
         .then(function (message) {
           message.react('⭕');
@@ -49,33 +49,24 @@ client.on('message', msg => {
     }
   } 
 });
-
-client.on('message', msg => {
-  if (msg.content === '!quiz') {
-    msg.channel.send(item.question).then(() => {
-      msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
-        .then(collected => {
-          msg.channel.send(`${collected.first().author} дал правильный ответ!`);
-        })
-        .catch(collected => {
-          msg.channel.send('Извиняй, но время прошло.');
-        });
-    });
-  }
-})
+*/
 
 client.on('message', function(message) {
   if (message.content == "!clear") {
-    if (message.member.roles.find(r => r.name === "Owner")) {
-      if (message.member.hasPermission("MANAGE_MESSAGES")) {
-          message.channel.fetchMessages()
-             .then(function(list){
-                  message.channel.bulkDelete(list);
-              }, function(err){message.channel.send("У вас нету прав на выполнение данного действия.")})
-          }                        
-      }
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+      message.channel.send('Через 30 хвилин буде очищено чат.');
+      setTimeout(function(){ 
+        message.channel.fetchMessages()
+        .then(function(list){
+          message.channel.bulkDelete(list);
+	}
+      }, 300000);   
+    }   
+    else {
+      message.channel.send("Error! Check Permissions")
+      message.delete(5000);
+    }
   }
-
 });
 
 client.on('message', message => {
@@ -89,42 +80,8 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-  if (message.content === '!owner') {
+  if (message.content === 'BotCanSayButHeCanN\'tPromise') {
       message.channel.send('Вот владелец: https://steamcommunity.com/id/RedJoar');
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!servers') {
-      message.channel.send('В данный момент сервера закрыти.');
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!win') {
-      const attachment = new Attachment('http://mrwgifs.com/wp-content/uploads/2014/11/HQ-Carlton-Dance-With-a-Transparent-Background-Gif-From-The-Fresh-Prince-Of-Bel-Air.gif');
-      message.channel.send(attachment);
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!nyancat') {
-      const attachment = new Attachment('https://media2.giphy.com/media/mTs11L9uuyGiI/giphy.gif');
-      message.channel.send(attachment);
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!nice') {
-      const attachment = new Attachment('https://media1.giphy.com/media/n0lnosk5JX8Dm/giphy.gif');
-      message.channel.send(attachment);
-  }
-});
-
-client.on('message', message => {
-  if (message.content === '!fuckyou') {
-      const attachment = new Attachment('http://24.media.tumblr.com/69cc630a0001996cc58983c391f10407/tumblr_mr9x8jnd1I1semgkco1_500.gif');
-      message.channel.send(attachment);
   }
 });
 
